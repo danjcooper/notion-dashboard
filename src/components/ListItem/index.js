a464aa68b98a5aef0ee1ae138c9f1d9d0300d656
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2022-03-16 21:17:07 +0000

    start interactive list

---
+++ src/components/ListItem/index.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const ListItem = ({ item }) => {
+  const [active, setActive] = useState(true);
+  const [opacity, setOpacity] = useState('1');
+
+  const defaultStyles = {
+    background: 'rgb(168, 212, 168)',
+    width: '80%',
+    margin: '5px',
+    padding: '3px',
+    'border-radius': '5px',
+    display: 'inline-block',
+    opacity: opacity,
+  };
+
+  const handleClick = () => {
+    setActive(!active);
+    setOpacity((prevState) => (prevState === '1' ? '0.4' : '1'));
+  };
+
+  return (
+    <article style={defaultStyles} onClick={handleClick}>
+      <h2>{item.properties.Name.title[0].plain_text}</h2>
+      <p>{item.properties.Department.multi_select[0].name}</p>
+    </article>
+  );
+};
+
+export default ListItem;
--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import { ListItem } from '../../components';
 
 const List = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await axios.get(
+        'https://portfolio-api-dc.herokuapp.com/meals/all'
+      );
+      console.log(data.data.data.results);
+      setData(data.data.data.results);
+    };
+    getData();
+  }, []);
+
   return (
     <>
       <h1>List</h1>
+      {data ? data.map((i) => <ListItem key={i.id} item={i} />) : null}
     </>
   );
 };
