dac8c0150b086bd3ea14a784017c111d70c758b7
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2022-03-17 18:07:06 +0000

    conditional formatting

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react';
 
 const ListItem = ({ item }) => {
   const [active, setActive] = useState(true);
-  const [opacity, setOpacity] = useState('1');
+  const [opacity, setOpacity] = useState(
+    item.properties['Picked Up'].checkbox ? '0.4' : '1'
+  );
 
   const defaultStyles = {
     background: 'rgb(168, 212, 168)',
     width: '80%',
     margin: '5px',
     padding: '3px',
-    'border-radius': '5px',
+    borderRadius: '5px',
     display: 'inline-block',
     opacity: opacity,
   };
@@ -23,6 +25,9 @@ const ListItem = ({ item }) => {
     <article style={defaultStyles} onClick={handleClick}>
       <h2>{item.properties.Name.title[0].plain_text}</h2>
       <p>{item.properties.Department.multi_select[0].name}</p>
+      {item.properties.Notes.rich_text.length > 0 ? (
+        <p>{item.properties.Notes.rich_text[0].plain_text}</p>
+      ) : null}
     </article>
   );
 };
